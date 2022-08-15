12123a9394299d3c2e5c93ee794bea1dc62c1e45
Тимур Кадымов <tkadymov95@gmail.com>
2022-08-15 23:35:13 +0500

    Задача 1 , мили за рубли

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args) {
+        int amount = 120000;
+        int percent = 5;
+        int bonus = amount * percent / 100;
+        System.out.println("Итоговый бонус: " + bonus);
+
+
+
+    }
+}
